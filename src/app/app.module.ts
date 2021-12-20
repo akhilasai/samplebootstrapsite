66f65cae8f7faaf9b2f083980369eea84423af68
Akhila Sai Thumma <athumma@innominds.com>
2021-12-20 13:40:10 +0530

    DONE with 50%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { SliderComponent } from './slider/slider.component';
+import { MiddleComponent } from './middle/middle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ServicesComponent,
+    SliderComponent,
+    MiddleComponent
   ],
   imports: [
     BrowserModule,
